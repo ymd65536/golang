7da4c6787c87086274d95217ccce18917f3ded06
ymdcode999 <masterhinagiku@gmail.com>
2020-04-05 18:13:40 +0900

    エラー判定を修正

--- chat/main.go
+++ chat/main.go
@@ -34,7 +34,7 @@ func main() {
 	go r.run()
 	log.Println("Webサーバーを開始します。ポート： ", *addr)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }

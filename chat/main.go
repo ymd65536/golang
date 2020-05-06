0541aa732e54a0ca80239b8cf7ea9c97691eb1b6
KYamadaK <masterhinagiku@gmail.com>
2020-05-06 17:53:30 +0900

    Add Auth google LoginHandler

--- chat/main.go
+++ chat/main.go
@@ -7,6 +7,13 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/objx"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -21,15 +28,32 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			template.Must(template.ParseFiles(filepath.Join("templates",
 				t.filename)))
 	})
-	t.temp1.Execute(w, req)
+	data := map[string]interface{}{
+		"Host": req.Host,
+	}
+	if authCookie, err := req.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.temp1.Execute(w, data)
 }
 
 func main() {
 	//ポート番号を自由に設定できるようにする。
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	var clientID = ""
+	var key = ""
+
+	gomniauth.SetSecurityKey(key)
+	gomniauth.WithProviders(
+		facebook.New(clientID, key, "http://localhost:8080/auth/callback/facebook/"),
+		github.New(clientID, key, "http://localhost:8080/auth/callback/github/"),
+		google.New(clientID, key, "http://localhost:8080/auth/callback/google/"),
+	)
+
 	r := newRoom()
 	http.Handle("/chat", mustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	go r.run()
